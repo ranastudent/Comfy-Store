613fde599c1fa4b9d9d7b1b7da9df4c7a02645eb
Rana <reduanulislam92665@gmail.com>
2025-02-09 00:52:02 +0600

    toggle theme icon

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,9 +1,15 @@
 import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
+import NavLinks from './NavLinks';
+import { useState } from 'react';
 
 
 const Navbar = () => {
+      const [theme, setTheme] = useState(false)
+      const handleTheme = ()=>{
+            setTheme(!theme);
+      }
       return (
             <nav className='bg-base-200'>
                   <div className="navbar align-element">
@@ -21,20 +27,27 @@ const Navbar = () => {
                                           tabIndex={0}
                                           className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52 space-y-3'
                                     >
-                                          <li>Nav</li>
-                                          <li>Link</li>
+                                         
+                                         <NavLinks></NavLinks>
                                     </ul>
                               </div>
                         </div>
                         <div className="navbar-center hidden lg:flex">
                               <ul className='menu menu-horizontal space-x-3'>
                                     
-                                    <li>Nav</li>
-                                    <li>Link</li>
+                                   
+                                   <NavLinks></NavLinks>
                               </ul>
                         </div>
                         <div className="navbar-end"></div>
                         {/* THEME SETUP */}
+                        <label className='swap swap-rotate'>
+                              <input type='checkbox' onChange={handleTheme} />
+                              {/* SUN ICON */}
+                              <BsSunFill className='swap-on h-4 w-4' />
+                              {/* MOON ICON */}
+                              <BsMoonFill className='swap-off h-4 w-4' />
+                        </label>
                         {/* CART LINK */}
                         <NavLink to={"/cart"} className="btn btn-ghost btn-circle btn-md ml-4">
                               <div className="indicator">
